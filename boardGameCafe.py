75d6a66170f4927470107579ed3a259fac14731c
Annika Brown <114958568+annibrown@users.noreply.github.com>
2024-12-11 23:54:20 -0800

    Update boardGameCafe.py

--- boardGameCafe.py
+++ boardGameCafe.py
@@ -5,17 +5,6 @@ from database import database
 # global variables
 db_ops = database("cafe.db")
 
-# start screen
-# user can 
-    # make reservation
-    # order food
-    # order board game
-    # view past orders
-
-# main menu
-    # sign in
-    # create acct
-
 def options():
     print('''Would you like to
             1. Sign In
@@ -23,6 +12,7 @@ def options():
             3. Exit''')
     return helper.get_choice([1,2,3])
 
+# user wants to create an account
 def create_account():
     new_name = input("Please enter your name:\n")
     new_email = input("Please enter you email:\n")
@@ -30,6 +20,7 @@ def create_account():
     print("Your ID is: " + str(db_ops.get_id()))
     print("Save this!! You will use it to log in!")
 
+# user wants to sign in
 def user_menu():
     # Ask for ID and email to log in
     print("What is your ID number?")
@@ -39,13 +30,36 @@ def user_menu():
     IDvalidity = db_ops.check_id(entered_id)
 
     if IDvalidity:
-        while True:
+        signed_in = True
+        while signed_in:
             choice = menu_options()
+            if choice == 1:
+                view_menu()
+            if choice == 2:
+                view_board_games()
+            if choice == 3:
+                make_reservation(entered_id)
+            if choice == 4:
+                view_reservations(entered_id)
+            if choice == 5:
+                account_info(entered_id)
+    else:
+        print("Not a valid ID number. Try logging in with a valid one.")
 
-#def menu_options():
+# options on main screen
+def menu_options():
+    print('''Where would you like to go?
+            1. Menu
+            2. Board Games
+            3. Reserve
+            4. Reservations
+            5. Account
+          ''')
+    return helper.get_choice([1,2,3,4.5])
 
-# User wants to order food
-def order_food():
+# user wants to view_menu
+def view_menu():
+    print("Here is a list of all our drinks!")
     # show the menu
     food_menu_query = f"""
     SELECT *
@@ -54,8 +68,10 @@ def order_food():
     items = db_ops.select_query(food_menu_query)
     helper.pretty_print(items)
 
-def order_board_game():
-    # board game menu
+# user wants to view board games
+def view_board_games():
+    print("Here is a list of all our games!")
+    # show board game menu
     game_menu_query = f"""
     SELECT *
     FROM BoardGames;
@@ -63,15 +79,100 @@ def order_board_game():
     games = db_ops.select_query(game_menu_query)
     helper.pretty_print(games)
 
-#def make_reservation():
+def make_reservation(id):
+
+    # asks for reservation date
+    failed = True
+    while failed:
+        failed = False
+        reserve_date = input("What day would you like to make a reservation? (YYYY-MM-DD)\n")
+        if len(reserve_date) != 10:
+            failed = True
+        else:
+            for i in range(len(reserve_date)):
+                if i == 4 or i == 7:
+                    if reserve_date[i] != '-':
+                        failed = True
+                else:
+                    try:
+                        int(reserve_date[i])
+                    except:
+                        failed = True
+        if failed == False:
+            if int(reserve_date[:4]) < 2024 or int(reserve_date[5:7]) > 12 or int(reserve_date[8:]) > 30:
+                failed = True
+        if failed:
+            print("Invalid entry. Please enter a valid date in the correct format (YYYY-MM-DD)")
+
+    # asks for reservation time
+    failed = True
+    while failed:
+        failed = False
+        reserve_time = input("What time would you like your reservation to be? (##:##)\n")
+        if len(reserve_time) != 5:
+            failed = True
+        else:
+            if reserve_time[2] != ':':
+                failed = True
+            else:
+                try:
+                    if int(reserve_time[:2]) > 23 or int(reserve_time[3:]) > 59:
+                        failed = True
+                except:
+                    failed = True
+        if failed:
+            print("Invalid entry. Please enter a valid time in the correct format")
+
+    # asks for guest number
+    failed = True
+    while failed:
+        failed = False
+        guest_amount = input("How many people will there be (including yourself)\n")
+        try:
+            int(guest_amount)
+        except:
+            failed = True
+        if failed:
+            print("Invalid. Please enter an integer")
+
+    # allows them to pre-order drinks
+    # print("Would you like to order drinks ahead of time?\n")
+    # print('''1. Yes
+    #          2. No
+    #          ''')
+    # if helper.get_choice([1,2]) == 1:
+        
+    #print("Would you like to reserve a game?")
+    #print("What game would you like to reserve?")
+
+    # adds new reservation to table
+    db_ops.create_new_reservation(id, reserve_date, reserve_time, guest_amount)
+
+# allows user to view their reservations
+def view_reservations(id):
+    reservations_query = f'''
+    SELECT *
+    FROM Reservation
+    WHERE customerID LIKE "{id}";
+    '''
+    results = db_ops.select_query(reservations_query)
+    print("Here are all of your reservations:\n")
+    helper.pretty_print(results)
 
 #main method
-#db_ops.create_Customers_table()
-#db_ops.create_BoardGames_table()
-#db_ops.create_MenuItems_table()
-#db_ops.create_Reservations_table()
-#db_ops.create_BoardGameOrders_table()
-#db_ops.create_MenuOrders_table()
+db_ops.create_Customers_table()
+db_ops.create_BoardGames_table()
+db_ops.create_MenuItems_table()
+db_ops.create_Reservations_table()
+db_ops.create_BoardGameOrders_table()
+db_ops.create_MenuOrders_table()
+
+db_ops.populate_table("Customers", "Customers.csv")
+db_ops.populate_table("BoardGames", "BoardGames.csv")
+db_ops.populate_table("MenuItems", "MenuItems.csv")
+db_ops.populate_table("Reservations", "Reservations.csv")
+db_ops.populate_table("BoardGameOrders", "BoardGameOrders.csv")
+db_ops.populate_table("MenuOrders", "MenuOrders.csv")
 
 #testing
 db_ops.create_new_customer("alex", "lark@chapman.edu")
