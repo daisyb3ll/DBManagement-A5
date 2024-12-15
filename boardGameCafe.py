978c4c058ee455f07dd304a44937cf2762075ae1
Savana Chou <sachou@chapman.edu>
2024-12-15 15:50:33 -0800

    moved daisy connection to main

--- boardGameCafe.py
+++ boardGameCafe.py
@@ -1,259 +1,349 @@
 # imports
-from helper import helper
+from flask import Flask, request, render_template, jsonify
 from database import database
+from helper import helper
 
-# global variables
+app = Flask(__name__)
+
+# import csv
+# from helper import helper
+# from database import database
+
+
+# Global database object
 db_ops = database("cafe.db")
 
-def options():
-    print('''Would you like to
-            1. Sign In
-            2. Create Account
-            3. Exit''')
-    return helper.get_choice([1,2,3])
+# Initialize database tables
+@app.route('/initialize', methods=['GET'])
+def initialize_database():
+    db_ops.create_Customers_table()
+    db_ops.create_BoardGames_table()
+    db_ops.create_MenuItems_table()
+    db_ops.create_Reservations_table()
+    db_ops.create_BoardGameOrders_table()
+    db_ops.create_MenuOrders_table()
+    db_ops.populate_table("Customers", "Customers.csv")
+    db_ops.populate_table("BoardGames", "BoardGames.csv")
+    db_ops.populate_table("MenuItems", "MenuItems.csv")
+    db_ops.populate_table("Reservations", "Reservations.csv")
+    db_ops.populate_table("BoardGameOrders", "BoardGameOrders.csv")
+    db_ops.populate_table("MenuOrders", "MenuOrders.csv")
+    return "Database initialized and populated!"
+
+
+# Home page
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+# @app.route('/sign-in')
+# def sign_in():
+#     return render_template('sign-in.html')
+
+@app.route('/')
+def menu():
+        return render_template('menu.html')
+
 
 # user wants to create an account
+@app.route('/create-account', methods = ['GET','POST'])
 def create_account():
-    new_name = input("Please enter your name:\n")
-    new_email = input("Please enter you email:\n")
-    db_ops.create_new_customer(new_name, new_email)
-    print("Your ID is: " + str(db_ops.get_id()))
-    print("Save this!! You will use it to log in!")
+    if request.method == 'POST':
+        new_name = request.form['name']
+        new_email = request.form['email']
+        db_ops.create_new_customer(new_name, new_email)
+        customer_id = db_ops.get_customer_id(new_name, new_email)
+        return jsonify({
+            "message": "Account created successfully!",
+            "customer_id": customer_id
+        })
+    return render_template('create-account.html')
 
-# user wants to sign in
-def user_menu():
-    # Ask for ID and email to log in
-    print("What is your ID number?")
-    entered_id = input("ID Number: ")
+# @app.route('/reserve', methods=['POST'])
+# def make_reservation():
+#     customer_id = request.form['customer_id']
+#     reserve_date = request.form['date']
+#     reserve_time = request.form['time']
+#     guest_count = request.form['guest_count']
 
-    # Check to see if ID number is valid and log in
-    IDvalidity = db_ops.check_id(entered_id)
+#     # Print debug info to confirm input data
+#     print(f"Reservation received: Customer ID = {customer_id}, Date = {reserve_date}, Time = {reserve_time}, Guests = {guest_count}")
 
-    if IDvalidity:
-        signed_in = True
-        while signed_in:
-            choice = menu_options()
-            if choice == 1:
-                view_menu()
-            if choice == 2:
-                view_board_games()
-            if choice == 3:
-                make_reservation(entered_id)
-            if choice == 4:
-                view_reservations(entered_id)
-            if choice == 5:
-                account_info(entered_id)
-    else:
-        print("Not a valid ID number. Try logging in with a valid one.")
+#     db_ops.create_new_reservation(customer_id, reserve_date, reserve_time, guest_count)
+#     return jsonify({"message": "Reservation created successfully!"})
+
+
+    # local implementation
+    # def options():
+    #     print('''Would you like to
+    #             1. Sign In
+    #             2. Create Account
+    #             3. Exit''')
+    #     return helper.get_choice([1,2,3])
+
+    # new_name = input("Please enter your name:\n")
+    # new_email = input("Please enter you email:\n")
+    # db_ops.create_new_customer(new_name, new_email)
+    # print("Your ID is: " + str(db_ops.get_id()))
+    # print("Save this!! You will use it to log in!")
+
+
+
+
+    # Sign in
+@app.route('/sign-in', methods=['GET', 'POST'])
+def sign_in():
+    if request.method == 'POST':
+        entered_id = request.form['customer_id']
+        IDvalidity = db_ops.check_customer_id(entered_id)
+        if IDvalidity:
+            return render_template('user-menu.html', customer_id=entered_id)
+        else:
+            return jsonify({"error": "Invalid ID. Please try again!"})
+    return render_template('sign-in.html')
+
+    #local implementation: 
+    # # user wants to sign in
+    # def user_menu():
+    #     # Ask for ID and email to log in
+    #     print("What is your ID number?")
+    #     entered_id = input("ID Number: ")
+
+    # # Check to see if ID number is valid and log in
+    # IDvalidity = db_ops.check_id(entered_id)
+
+    # if IDvalidity:
+    #     signed_in = True
+    #     while signed_in:
+    #         choice = menu_options()
+    #         if choice == 1:
+    #             view_menu()
+    #         if choice == 2:
+    #             view_board_games()
+    #         if choice == 3:
+    #             make_reservation(entered_id)
+    #         if choice == 4:
+    #             view_reservations(entered_id)
+    #         if choice == 5:
+    #             account_info(entered_id)
+    # else:
+    #     print("Not a valid ID number. Try logging in with a valid one.")
 
 # options on main screen
-def menu_options():
-    print('''Where would you like to go?
-            1. Menu
-            2. Board Games
-            3. Reserve
-            4. Reservations
-            5. Account
-          ''')
-    return helper.get_choice([1,2,3,4.5])
 
-# user wants to view_menu
+
+# User menu
+@app.route('/user-menu/<customer_id>')
+def user_menu(customer_id):
+    return render_template('user-menu.html', customer_id=customer_id)
+
+# def menu_options():
+#     print('''Where would you like to go?
+#             1. Menu
+#             2. Board Games
+#             3. Reserve
+#             4. Reservations
+#             5. Account
+#         ''')
+#     return helper.get_choice([1,2,3,4.5])
+
+
+# View menu
+@app.route('/view-menu', methods=['GET'])
 def view_menu():
-    print("Here is a list of all our drinks!")
-    # show the menu
-    food_menu_query = f"""
-    SELECT *
-    FROM MenuItems;
-    """
+    food_menu_query = "SELECT * FROM MenuItems;"
     items = db_ops.select_query(food_menu_query)
-    helper.pretty_print(items)
+    return render_template('menu.html', items=items)
 
-# user wants to view board games
+# # user wants to view_menu
+# def view_menu():
+#     print("Here is a list of all our drinks!")
+#     # show the menu
+#     food_menu_query = f"""
+#     SELECT *
+#     FROM MenuItems;
+#     """
+#     items = db_ops.select_query(food_menu_query)
+#     helper.pretty_print(items)
+
+
+# View board games
+@app.route('/board-games', methods=['GET'])
 def view_board_games():
-    print("Here is a list of all our games!")
-    # show board game menu
-    game_menu_query = f"""
-    SELECT *
-    FROM BoardGames;
-    """
+    game_menu_query = "SELECT * FROM BoardGames;"
     games = db_ops.select_query(game_menu_query)
-    helper.pretty_print(games)
+    return render_template('board-games.html', games=games)
 
-def make_reservation(id):
+# # user wants to view board games
+# def view_board_games():
+#     print("Here is a list of all our games!")
+#     # show board game menu
+#     game_menu_query = f"""
+#     SELECT *
+#     FROM BoardGames;
+#     """
+#     games = db_ops.select_query(game_menu_query)
+#     helper.pretty_print(games)
 
-    # asks for reservation date
-    failed = True
-    while failed:
-        failed = False
-        reserve_date = input("What day would you like to make a reservation? (YYYY-MM-DD)\n")
-        if len(reserve_date) != 10:
-            failed = True
-        else:
-            for i in range(len(reserve_date)):
-                if i == 4 or i == 7:
-                    if reserve_date[i] != '-':
-                        failed = True
-                else:
-                    try:
-                        int(reserve_date[i])
-                    except:
-                        failed = True
-        if failed == False:
-            if int(reserve_date[:4]) < 2024 or int(reserve_date[5:7]) > 12 or int(reserve_date[8:]) > 30:
-                failed = True
-        if failed:
-            print("Invalid entry. Please enter a valid date in the correct format (YYYY-MM-DD)")
 
-    # asks for reservation time
-    failed = True
-    while failed:
-        failed = False
-        reserve_time = input("What time would you like your reservation to be? (##:##)\n")
-        if len(reserve_time) != 5:
-            failed = True
-        else:
-            if reserve_time[2] != ':':
-                failed = True
-            else:
-                try:
-                    if int(reserve_time[:2]) > 23 or int(reserve_time[3:]) > 59:
-                        failed = True
-                except:
-                    failed = True
-        if failed:
-            print("Invalid entry. Please enter a valid time in the correct format")
+# Make reservation
+@app.route('/reserve', methods=['POST'])
+def make_reservation():
+    print(request.form)  # Debugging
+    customer_id = request.form.get('customer_id')  # Safely get customer_id
+    reserve_date = request.form.get('date')
+    reserve_time = request.form.get('time')
+    guest_count = request.form.get('guestCount')
 
-    # asks for guest number
-    failed = True
-    while failed:
-        failed = False
-        guest_amount = input("How many people will there be (including yourself)\n")
-        try:
-            int(guest_amount)
-        except:
-            failed = True
-        if failed:
-            print("Invalid. Please enter an integer")
+    if not all([customer_id, reserve_date, reserve_time, guest_count]):
+        return jsonify({"error": "Missing required fields"}), 400
 
-    # adds new reservation to table
-    db_ops.create_new_reservation(id, reserve_date, reserve_time, guest_amount)
+    db_ops.create_new_reservation(customer_id, reserve_date, reserve_time, guest_count)
+    return jsonify({"message": "Reservation created successfully!"})
 
-    # FOR ALEX TO MAKE
-    r_ID = db_ops.get_reservation_id()
 
-    # allows them to pre-order drinks
-    print("Would you like to order drinks ahead of time?\n")
-    print('''1. Yes
-             2. No
-            ''')
-    if helper.get_choice([1,2]) == 1:
-        view_menu()
-        done_ordering = False
-        while done_ordering == False:
-            drink_order = input("Enter the ID of the drink you would like\n")
-            print('''Which milk would you like?
-                     1. Regular
-                     2. Dairy free option
-                    ''')
-            vegan = helper.get_choice([1,2])
-            try:
-                if vegan == 1:
-                    db_ops.order_boba(r_ID, drink_order, "none")
-                elif vegan == 2:
-                    db_ops.order_boba(r_ID, drink_order, "dairy free")
-            except:
-                print("Invalid drink ID, your request didn't go through\n")
-            print('''Would you like to order another drink?
-                  1. Yes
-                  2. No
-                  ''')
-            if helper.get_choice([1,2]) == 2:
-                done_ordering = True
+
+
+
+# def make_reservation(id):
+
+#     # asks for reservation date
+#     failed = True
+#     while failed:
+#         failed = False
+#         reserve_date = input("What day would you like to make a reservation? (YYYY-MM-DD)\n")
+#         if len(reserve_date) != 10:
+#             failed = True
+#         else:
+#             for i in range(len(reserve_date)):
+#                 if i == 4 or i == 7:
+#                     if reserve_date[i] != '-':
+#                         failed = True
+#                 else:
+#                     try:
+#                         int(reserve_date[i])
+#                     except:
+#                         failed = True
+#         if failed == False:
+#             if int(reserve_date[:4]) < 2024 or int(reserve_date[5:7]) > 12 or int(reserve_date[8:]) > 30:
+#                 failed = True
+#         if failed:
+#             print("Invalid entry. Please enter a valid date in the correct format (YYYY-MM-DD)")
+
+#     # asks for reservation time
+#     failed = True
+#     while failed:
+#         failed = False
+#         reserve_time = input("What time would you like your reservation to be? (##:##)\n")
+#         if len(reserve_time) != 5:
+#             failed = True
+#         else:
+#             if reserve_time[2] != ':':
+#                 failed = True
+#             else:
+#                 try:
+#                     if int(reserve_time[:2]) > 23 or int(reserve_time[3:]) > 59:
+#                         failed = True
+#                 except:
+#                     failed = True
+#         if failed:
+#             print("Invalid entry. Please enter a valid time in the correct format")
+
+#     # asks for guest number
+#     failed = True
+#     while failed:
+#         failed = False
+#         guest_amount = input("How many people will there be (including yourself)\n")
+#         try:
+#             int(guest_amount)
+#         except:
+#             failed = True
+#         if failed:
+#             print("Invalid. Please enter an integer")
+
+#     # allows them to pre-order drinks
+#     # print("Would you like to order drinks ahead of time?\n")
+#     # print('''1. Yes
+#     #          2. No
+#     #          ''')
+#     # if helper.get_choice([1,2]) == 1:
         
-    print('''Would you like to reserve a game?
-          1. Yes
-          2. No
-          ''')
-    if helper.get_choice([1,2]) == 1:
-        view_board_games()
-        game_error = False
-        while game_error == False:
-            game_order = input("What game would you like to reserve?\n")
-            try:
-                db_ops.reserve_board_game(r_ID, game_order)
-                game_error = False
-            except:
-                print("Invalid game name, your request didn't go through")
-                game_error = True
+#     #print("Would you like to reserve a game?")
+#     #print("What game would you like to reserve?")
 
-
-    
+#     # adds new reservation to table
+#     db_ops.create_new_reservation(id, reserve_date, reserve_time, guest_amount)
 
 # allows user to view their reservations
-def view_reservations(id):
+
+
+# View reservations
+@app.route('/reservations/<customer_id>', methods=['GET'])
+def view_reservations(customer_id):
     reservations_query = f'''
-    SELECT reservationID
-    FROM Reservation
-    WHERE customerID LIKE "{id}";
+    SELECT *
+    FROM Reservations
+    WHERE customerID = ?;
     '''
-    results = db_ops.select_query(reservations_query)
-    print("Here are all of your reservations:\n")
+    results = db_ops.cursor.execute(reservations_query, (customer_id,)).fetchall()
+    return render_template('reservations.html', reservations=results, customer_id=customer_id)
 
-    for i in results:
-        db_ops.view_reservations(i)
-
-    # allows user to delete reservation
-    print('''Would you like to cancel a reservation?
-            1. Yes
-            2. No
-          ''')
-    delete_reservation_choice = helper.get_choice([1,2])
-    if delete_reservation_choice == 1:
-        try:
-            reservation_to_delete = int(input("Enter the reservation ID of the reservation you would like to cancel\n"))
-            db_ops.cancel_reservation(reservation_to_delete)
-        except:
-            print("invalid reservation ID")
-
-# allows the user to view info for their account
-def account_info(id):
-    account_query = f'''
-        SELECT *
-        FROM Customers
-        WHERE customerID LIKE "{id}";
-        '''
-    account_results = db_ops.select_query(account_query)
-    helper.pretty_print(account_results)
-
+# def view_reservations(id):
+#     reservations_query = f'''
+#     SELECT *
+#     FROM Reservation
+#     WHERE customerID LIKE "{id}";
+#     '''
+#     results = db_ops.select_query(reservations_query)
+#     print("Here are all of your reservations:\n")
+#     helper.pretty_print(results)
 
 #main method
-db_ops.create_Customers_table()
-db_ops.create_BoardGames_table()
-db_ops.create_MenuItems_table()
-db_ops.create_Reservations_table()
-db_ops.create_BoardGameOrders_table()
-db_ops.create_MenuOrders_table()
 
-db_ops.populate_table("Customers", "Customers.csv")
-db_ops.populate_table("BoardGames", "BoardGames.csv")
-db_ops.populate_table("MenuItems", "MenuItems.csv")
-db_ops.populate_table("Reservations", "Reservations.csv")
-db_ops.populate_table("BoardGameOrders", "BoardGameOrders.csv")
-db_ops.populate_table("MenuOrders", "MenuOrders.csv")
 
-#testing
-db_ops.create_new_customer("alex", "lark@chapman.edu")
-print(db_ops.get_customer_id("alex", "lark@chapman.edu"))
-print(db_ops.check_customer_id("1"))
+# Account info
+@app.route('/account/<customer_id>', methods=['GET'])
+def account_info(customer_id):
+    customer_query = '''
+    SELECT *
+    FROM Customers
+    WHERE customerID = ?;
+    '''
+    customer_info = db_ops.cursor.execute(customer_query, (customer_id,)).fetchone()
+    return render_template('account-info.html', customer=customer_info)
 
-while True:
-    user_choice = options()
-    if user_choice == 1:
-        user_menu()
-    if user_choice == 2:
-        create_account()
-    if user_choice == 3:
-        print("Bye bye! Come again soon!")
-        break
+if __name__ == '__main__':
+    app.run(debug=True)
 
-db_ops.destructor()
+
+
+# db_ops.create_Customers_table()
+# db_ops.create_BoardGames_table()
+# db_ops.create_MenuItems_table()
+# db_ops.create_Reservations_table()
+# db_ops.create_BoardGameOrders_table()
+# db_ops.create_MenuOrders_table()
+
+# db_ops.populate_table("Customers", "Customers.csv")
+# db_ops.populate_table("BoardGames", "BoardGames.csv")
+# db_ops.populate_table("MenuItems", "MenuItems.csv")
+# db_ops.populate_table("Reservations", "Reservations.csv")
+# db_ops.populate_table("BoardGameOrders", "BoardGameOrders.csv")
+# db_ops.populate_table("MenuOrders", "MenuOrders.csv")
+
+# #testing
+# db_ops.create_new_customer("alex", "lark@chapman.edu")
+# print(db_ops.get_customer_id("alex", "lark@chapman.edu"))
+# print(db_ops.check_customer_id("1"))
+
+# while True:
+#     user_choice = options()
+#     if user_choice == 1:
+#         user_menu()
+#     if user_choice == 2:
+#         create_account()
+#     if user_choice == 3:
+#         print("Bye bye! Come again soon!")
+#         break
+
+# db_ops.destructor()
